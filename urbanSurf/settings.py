cf3a30e638cbe2662d235bc5c22948f61048e74c
Katie Maxwell <katiemaxwell10@hotmail.com>
2017-07-13 16:54:06 +0100

    heroku settings

--- urbanSurf/settings.py
+++ urbanSurf/settings.py
@@ -148,7 +148,8 @@ STRIPE_SECRET = os.getenv('STRIPE_SECRET_KEY')
 STATICFILES_DIRS = (
    os.path.join(BASE_DIR, "static"),
 )
-STATIC_URL = '/static/'
+
+# STATIC_URL = '/static/'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
