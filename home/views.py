fea290c6d8e7bd2eee7cb413b6a9241ea9969fdb
Katie Maxwell <katiemaxwell10@hotmail.com>
2017-07-18 19:43:35 +0100

    read more button for blog

--- home/views.py
+++ home/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.utils import timezone
 
 # Create your views here.
 def get_index(request):
     return render(request, 'index.html')
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, pk=id)
+    post.views += 1
+    post.save()
+    return render(request, "postdetail.html", {'post': post})
 
 def top3_post_list(request):
 
