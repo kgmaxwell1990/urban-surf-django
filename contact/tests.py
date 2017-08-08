353aa9228c84158b4e5d4a8275ed76693accad28
Katie Maxwell <katiemaxwell10@hotmail.com>
2017-08-08 17:02:36 +0100

    automated testing

--- contact/tests.py
+++ contact/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .views import contact
+from django.core.urlresolvers import resolve
 
-# Create your tests here.
+ 
+class ContactTest(TestCase):
+    def test_contact_resolves(self):
+        contact_page = resolve('/contact/')
+        self.assertEqual(contact_page.func, contact)
+
+
+    def test_contact_status_code_is_ok(self):
+        contact_page = self.client.get('/contact/')
+        self.assertEqual(contact_page.status_code, 200)
+
+
+
