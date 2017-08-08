38cf026f37999b09a3324dd733fe7f44011e3833
Katie Maxwell <katiemaxwell10@hotmail.com>
2017-08-08 20:20:50 +0100

    live email settings

--- contact/email_credentials.py
+++ contact/email_credentials.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True       
 EMAIL_HOST = 'smtp.gmail.com'      
 EMAIL_PORT = 587      
-EMAIL_HOST_USER = 'kgmaxwell1990@gmail.com'       
-EMAIL_HOST_PASSWORD = 'Manuchao16'
+EMAIL_HOST_USER = 'kmaxwellwebdev@gmail.com'       
+EMAIL_HOST_PASSWORD = 'Manuchao17'
